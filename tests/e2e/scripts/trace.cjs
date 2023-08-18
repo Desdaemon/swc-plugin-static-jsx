b75c284eed683ffe9f2e2b6305e1e68c38df7637
Viet Dinh <54ckb0y789@gmail.com>
2023-08-17 22:11:29 -0400

    chore: remove min profile

--- tests/e2e/scripts/trace.cjs
+++ tests/e2e/scripts/trace.cjs
@@ -1,17 +1,17 @@
 #!/usr/bin/env node
 
-const { transformFile, __experimental_registerGlobalTraceConfig } = require('@swc/core')
+const { transformFile, __experimental_registerGlobalTraceConfig } = require("@swc/core");
 
 __experimental_registerGlobalTraceConfig({
-  type: "traceEvent",
-  fileName: '.trace.json'
+	type: "traceEvent",
+	fileName: ".trace.json",
 });
 
-transformFile('main.test.js', {
-  cwd: require.resolve(".."),
-  jsc: {
-    experimental: {
-      plugins: [[require.resolve('swc-plugin-static-jsx'), {}]]
-    }
-  }
-})
+transformFile("main.test.js", {
+	cwd: require.resolve(".."),
+	jsc: {
+		experimental: {
+			plugins: [[require.resolve("swc-plugin-static-jsx"), {}]],
+		},
+	},
+});
